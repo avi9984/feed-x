a61e3e2a98543e1f3134c4870534f0bd07cac77f
avi9984 <avinashkumar151199@gmail.com>
2026-09-20 15:25:00 +0530

    admin model done

---
+++ models/admin.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    mobile_no: { type: String, required: true, unique: true, minLength: 10, maxLength: 10 },
+    password: { type: String, required: true },
+    login_history: { type: [Date.now()], default: [] }
+}, { versionKey: false, timestamps: true })
+
+const Admin = mongoose.model("fx_admin", adminSchema);
+
+export default Admin;
