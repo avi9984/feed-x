eb2623589ed1d7b652f26c32ba37571fdd5687ce
avi9984 <avinashkumar151199@gmail.com>
2026-09-21 22:47:42 +0530

    Changes in admin model for added the types for admin

--- models/admin.model.js
+++ models/admin.model.js
@@ -5,7 +5,8 @@ const adminSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     mobile_no: { type: String, required: true, unique: true, minLength: 10, maxLength: 10 },
     password: { type: String, required: true },
-    login_history: { type: [Date.now()], default: [] }
+    login_history: { type: [Date.now()], default: [] },
+    type: { type: String, enum: ["Admin"], default: "Admin" }
 }, { versionKey: false, timestamps: true })
 
 const Admin = mongoose.model("fx_admin", adminSchema);
