b396195fe0d8ce8f8be109fe9726d1d549999b44
avi9984 <avinashkumar151199@gmail.com>
2026-09-23 21:50:38 +0530

    Added the last login in admin schema

--- models/admin.model.js
+++ models/admin.model.js
@@ -6,6 +6,7 @@ const adminSchema = new mongoose.Schema({
     mobile_no: { type: String, required: true, unique: true, minLength: 10, maxLength: 10 },
     password: { type: String, required: true },
     login_history: { type: [Date.now()], default: [] },
+    last_login:{type:Date},
     type: { type: String, enum: ["Admin"], default: "Admin" }
 }, { versionKey: false, timestamps: true })
 
